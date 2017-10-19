bc92c8c68a6f409a4d20f7704a335838161b1c56
spartakusQ <spartakus1688@gmail.com>
2017-10-19 09:03:46 +0300

    Migration done

--- db/migrate/20171019060033_create_posts.rb
+++ db/migrate/20171019060033_create_posts.rb
@@ -3,9 +3,9 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :author, foreign_key: true
+      t.references :author
       t.datetime :published_at
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user
 
       t.timestamps
     end
